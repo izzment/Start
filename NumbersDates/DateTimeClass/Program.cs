6d21e7745a4cd29d614a022113f92478ac97a7d7
izzment <izzment@gmail.com>
2024-10-10 09:41:39 +0100

    finished datetime

--- NumbersDates/DateTimeClass/Program.cs
+++ NumbersDates/DateTimeClass/Program.cs
@@ -2,19 +2,28 @@
 // Example file for working with Dates and Times
 
 // TODO: Use DateTime.Now property to get the current date and time
-
+DateTime now = DateTime.Now;
+Console.WriteLine(now)
 
 // TODO: DateTime.Today gets just the current date with time set to 12:00:00 AM
-
+DateTime today = DateTime.Today;
+Console.WriteLine(today);
 
 // TODO: DateOnly and TimeOnly represent just dates and times
+DateOnly dt = DateOnly.FromDateTime(DateTime.Now);
+TimeOnly tm = TimeOnly.FromDateTime(DateTime.Now);
 
+Console.WriteLine(dt);
+Console.WriteLine(tm);
 
 // TODO: Dates have properties that can be inspected
-
+Console.WriteLine(today.DayOfWeek);
+Console.WriteLine(today.DayOfYear);
 
 // TODO: Dates also have methods to change their values
-
+now = now.AddDays(6);
+now = now.AddHours(12);
+now = now.Add
 
 // TODO: The TimeSpan class represents a duration of time
 // DateTime AprilFools = new DateTime(now.Year, 4, 1);

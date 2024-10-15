7365664625bb093ee03d41560600ac6d049209dc
izzment <izzment@gmail.com>
2024-10-15 13:00:15 +0100

    finished regex practice problems

--- Regex/Replacing/Program.cs
+++ Regex/Replacing/Program.cs
@@ -1,5 +1,6 @@
 ﻿// LinkedIn Learning Course .NET Programming with C# by Joe Marini
 // Example file for Replacing content with Regexes 
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
 string teststr1 = "The quick brown Fox jumps over the lazy Dog";
@@ -8,8 +9,21 @@ Regex CapWords = new Regex(@"[A-Z]\w+");
 
 // TODO: Regular expressions can be used to replace content in strings
 // in addition to just searching for content
-
+string result = CapWords.Replace(teststr1, "***");
+Console.WriteLine(teststr1);
+Console.WriteLine(result);
 
 // TODO: Replacement text can be generated on the fly using MatchEvaluator
 // This is a delegate that computes the new value of the replacement
 
+string MakeUpper(Match m) {
+    string s = m.ToString();
+    if (m.Index == 0){
+        return s;
+    }
+    return s.ToUpper();
+}
+
+string upperstr = CapWords.Replace(teststr1, new MatchEvaluator(MakeUpper));
+Console.WriteLine(teststr1);
+Console.WriteLine(upperstr);
